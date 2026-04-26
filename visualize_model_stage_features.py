208800d7e0e4b568f47b3c64df1a42399eaac862
zouhappyyy <zoule2020@163.com>
2026-04-27 00:12:00 +0800

    新增

--- visualize_model_stage_features.py
+++ visualize_model_stage_features.py
@@ -349,3 +349,11 @@ if __name__ == "__main__":
         save_raw=not args.no_save_raw,
         device=None if args.device in (None, "None") else args.device,
     )
+
+# python visualize_model_stage_features.py \
+#   --checkpoint_path ./ckpt/nnUNet/3d_fullres/Task530_EsoTJ_30pct/nnUNetTrainerV2_Double_CCA_UPSam_fd_loss_RWKV_MedNeXt__nnUNetPlansv2.1_trgSp_1x1x1_rwkv/fold_1 \
+#   --checkpoint_name model_best \
+#   --image ./ckpt/nnUNet/3d_fullres/Task530_EsoTJ_30pct/nnUNetTrainerV2_Double_CCA_UPSam_fd_loss_RWKV_MedNeXt__nnUNetPlansv2.1_trgSp_1x1x1_rwkv/fold_1/validation_raw/ESO_TJ_60011222468.nii.gz \
+#   --out_dir ./stage_feature_vis \
+#   --axis z \
+#   --topk 6
